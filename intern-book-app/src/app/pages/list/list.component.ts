1f9d54f9d169644f218d5e46bfa623d8e2a0cc8a
ArataYoshida <>
2022-04-20 18:42:23 +0900

    修正

--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -41,26 +41,27 @@ export class ListComponent implements OnInit {
 
   addBook(): void{
     //各ボックスが空欄ではないか
-    if(this.tmpBook.name != '' && this.tmpBook.detail != '' && this.tmpBook.evaluation != ''){
-      //数値であるか
-      if(typeof(this.tmpBook.evaluation) === "number"){
-        //0~100の範囲内か
-        if(this.tmpBook.evaluation < 100 && this.tmpBook.evaluation >= 0){
-          this.bookList.push(
-            {
-              name: this.tmpBook.name,
-              detail: this.tmpBook.detail,
-              evaluation: this.tmpBook.evaluation
-            }
-          )
-        }else{
-          alert("0以上100以下の数値にしてください")
-        }
-      }else{
-        alert("数字を入力してください")
-      }
-    }else{
+    if(this.tmpBook.name === '' || this.tmpBook.detail === '' || this.tmpBook.evaluation === ''){
       alert("全てのデータを入力してください")
+      return;
+    }
+    //0~100の範囲内か
+    if(this.tmpBook.evaluation > 100 || this.tmpBook.evaluation < 0){
+      alert("0以上100以下の数値にしてください")
+      return;
+    }
+    //数値であるか
+    if(typeof(this.tmpBook.evaluation) === "number"){
+      this.bookList.push(
+        {
+          name: this.tmpBook.name,
+          detail: this.tmpBook.detail,
+          evaluation: this.tmpBook.evaluation
+        }
+      )
+    }
+    else{
+      alert("評価点にアルファベットを含めないでください")
     }
 
   }
